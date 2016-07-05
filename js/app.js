0f6726bdc27e6208a2554218410628fdf6665260
Rhonda Goolsby <rlgoolsby@gmail.com>
2016-07-05 16:38:36 -0400

    Add folders and files

---
+++ js/app.js
@@ -0,0 +1,67 @@
+function OrderFormController($scope){
+
+
+    $scope.coffee= [
+
+        {
+          name: "Columbian",
+          price: 15,
+          active: true
+        },
+        {
+          name: "African",
+          price: 20,
+          active: false
+        },
+        {
+          name: "Indonesian",
+          price: 25,
+          active: false
+        },
+        {
+          name: "Peru",
+          price: 17,
+          active: false
+        }
+    ];
+
+    $scope.toggleActive = function(s){
+    		s.active = !s.active;
+    	};
+
+      $scope.total = function(){
+
+        var total = 0;
+
+        angular.forEach($scope.coffee, function(s){
+          if (s.active){
+            total+= s.price;
+          }
+        });
+
+      return total;
+
+      };
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
